dee68357ff9c352c1b1d140393fb046dd3138da6
nwu1015 <marchenkov3004@gmail.com>
2025-09-29 12:59:08 +0300

    Initial commit

---
+++ src/main/java/com/example/workfinder/WorkFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.workfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workfinder/WorkFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
